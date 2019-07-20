bd08082f41089b87100173bb1aa3bbf73ce6c582
Josh Yap <joshua.c.yap@gmail.com>
2019-07-20 13:56:59 -0500

    trying to figure out css gradient transitions

---
+++ colors.js
@@ -0,0 +1,16 @@
+// const contentBox = document.querySelector(".content-box");
+
+function makeOrange() {
+    // alert('working');
+    console.log(document.getElementsByClassName("container"));
+    // console.log(contentBox);
+    let x = document.getElementsByClassName("container");
+    x.style.backgroundColor = "purple";
+    // x.style.setProperty('--main-bg-color', "#FFA500");
+}
+
+
+
+// :root {
+//     --main-bg-color: #8EBA81;
+// }
