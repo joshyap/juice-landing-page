5a3c5abbc78d63384498332cd258e1a236bb66e4
Josh Yap <joshua.c.yap@gmail.com>
2019-07-20 21:57:31 -0500

    set up color switching

--- colors.js
+++ colors.js
@@ -1,12 +1,31 @@
 // const contentBox = document.querySelector(".content-box");
 
+function makeGreen() {
+    var elements = document.getElementsByClassName("content-box");
+    var singleNode = elements[0];
+    singleNode.style.backgroundColor = "#8EBA81";
+    console.log(singleNode.style);
+}
+
 function makeOrange() {
-    // alert('working');
-    console.log(document.getElementsByClassName("container"));
-    // console.log(contentBox);
-    let x = document.getElementsByClassName("container");
-    x.style.backgroundColor = "purple";
-    // x.style.setProperty('--main-bg-color', "#FFA500");
+    var elements = document.getElementsByClassName("content-box");
+    var singleNode = elements[0];
+    singleNode.style.backgroundColor = "orange";
+    console.log(singleNode.style);
+}
+
+function makePink() {
+    var elements = document.getElementsByClassName("content-box");
+    var singleNode = elements[0];
+    singleNode.style.backgroundColor = "pink";
+    console.log(singleNode.style);
+}
+
+function makePurple() {
+    var elements = document.getElementsByClassName("content-box");
+    var singleNode = elements[0];
+    singleNode.style.backgroundColor = "purple";
+    console.log(singleNode.style);
 }
 
 
@@ -14,3 +33,4 @@ function makeOrange() {
 // :root {
 //     --main-bg-color: #8EBA81;
 // }
+
